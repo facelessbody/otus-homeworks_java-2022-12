9580c3588edcc581394a0f688a264d9fcf682eda
nzhilik <kouk.official@gmail.com>
2023-03-19 06:58:17 +0300

    hw04: extreme optimization, try to not trigger gc

--- hw04-gc/src/main/java/ru/calculator/Summator.java
+++ hw04-gc/src/main/java/ru/calculator/Summator.java
@@ -1,21 +1,18 @@
 package ru.calculator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Summator {
     private int sum = 0;
     private int prevValue = 0;
     private int prevPrevValue = 0;
     private int sumLastThreeValues = 0;
     private int someValue = 0;
-    private final List<Data> listValues = new ArrayList<>();
+    private int listValuesSize = 0;
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
-        listValues.add(data);
-        if (listValues.size() % 6_600_000 == 0) {
-            listValues.clear();
+        ++listValuesSize;
+        if (listValuesSize % 6_600_000 == 0) {
+            listValuesSize = 0;
         }
         sum += data.getValue();
 
@@ -26,7 +23,7 @@ public class Summator {
 
         for (var idx = 0; idx < 3; idx++) {
             someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
-            someValue = Math.abs(someValue) + listValues.size();
+            someValue = Math.abs(someValue) + listValuesSize;
         }
     }
 
