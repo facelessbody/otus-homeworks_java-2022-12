6fe53968e3a7b09269c7eb4d1c0e1c6171b418a7
nzhilik <kouk.official@gmail.com>
2023-05-21 12:29:36 +0300

    hw12: add web server with file based auth

--- hw12-web-server/src/main/java/ru/otus/Application.java
+++ hw12-web-server/src/main/java/ru/otus/Application.java
@@ -1,21 +1,32 @@
 package ru.otus;
 
+import java.net.URL;
+
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jetty.security.HashLoginService;
 import org.hibernate.cfg.Configuration;
 import ru.otus.core.repository.DataTemplateHibernate;
 import ru.otus.core.repository.EntityClassesSupplier;
 import ru.otus.core.repository.HibernateUtils;
 import ru.otus.core.sessionmanager.TransactionManagerHibernate;
+import ru.otus.core.templateprocessor.TemplateProcessorImpl;
 import ru.otus.crm.db.migrations.MigrationsExecutorFlyway;
 import ru.otus.crm.model.Client;
 import ru.otus.crm.service.DbServiceClientImpl;
+import ru.otus.helpers.FileSystemHelper;
+import ru.otus.server.ClientsWebServer;
 
 @Slf4j
 public class Application {
-
+    private static final int WEB_SERVER_PORT = 8080;
+    private static final String TEMPLATES_DIR = "/templates/";
     public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
+    public static final String HASH_LOGIN_SERVICE_CONFIG_NAME = "realm.properties";
 
-    public static void main(String[] args) {
+    public static final String REALM_NAME = "AnyRealm";
+
+    public static void main(String[] args) throws Exception {
         var configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
 
         var dbUrl = configuration.getProperty("hibernate.connection.url");
@@ -32,6 +43,14 @@ public class Application {
         var clientTemplate = new DataTemplateHibernate<>(Client.class);
 ///
         var dbServiceClient = new DbServiceClientImpl(transactionManager, clientTemplate);
-        // todo: add business logic
+
+        var templatesProcessor = new TemplateProcessorImpl(TEMPLATES_DIR);
+
+        var loginService = new HashLoginService(REALM_NAME,
+                FileSystemHelper.localFileNameOrResourceNameToFullPath(HASH_LOGIN_SERVICE_CONFIG_NAME));
+        var clientsWebServer = new ClientsWebServer(WEB_SERVER_PORT, templatesProcessor, loginService, dbServiceClient);
+
+        clientsWebServer.start();
+        clientsWebServer.join();
     }
 }
